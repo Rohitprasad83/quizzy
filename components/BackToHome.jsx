700949be31d7a5fabb47865d1e30938899403fb7
Rohit Prasad <pdrohit674@gmail.com>
2023-04-16 14:47:54 +0530

    Remove blue background and added MaterialIcon

--- components/BackToHome.jsx
+++ components/BackToHome.jsx
@@ -1,10 +1,23 @@
 import { useNavigation } from '@react-navigation/native'
-import { View, Button } from 'react-native'
+import { StyleSheet, TouchableOpacity } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
 export function BackToHome() {
   const navigation = useNavigation()
   return (
-    <View>
-      <Button onPress={() => navigation.navigate('Home')} title="<-" />
-    </View>
+    <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+      <MaterialIcons
+        name="arrow-back"
+        size={30}
+        color="black"
+        style={{ marginLeft: 10, marginTop: 2 }}
+      />
+    </TouchableOpacity>
   )
 }
+
+const styles = StyleSheet.create({
+  backButton: {
+    backgroundColor: '#FFFFFF',
+    color: 'black',
+  },
+})
